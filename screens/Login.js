4034ddfe7bd3a6bb611330d45bbccaa93709f0bf
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-07-05 11:25:51 -0600

    creating login screen and registration screen

---
+++ screens/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native'
+import { Input, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Login = ({navigation}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Input
+                placeholder='Enter your email'
+                label='Email'
+                leftIcon={{ type: 'material', name: 'email' }}
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder='Enter your password'
+                label='Password'
+                leftIcon={{ type: 'material', name: 'lock' }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Button title='sign in' style={styles.button} />
+            <Button title='register' style={styles.button} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 100,
+    },
+    button: {
+        width: 370,
+        marginTop: 10
+    }
+});
---
+++ screens/Register.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native'
+import { Input, Button } from 'react-native-elements';
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [avatar, setAvatar] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <Input
+                placeholder='Enter your name'
+                label='Name'
+                value={name}
+                onChangeText={text => setName(text)}
+            />
+            <Input
+                placeholder='Enter your email'
+                label='Email'
+                value={email}
+                onChangeText={text => setEmail(text)}
+            />
+            <Input
+                placeholder='Enter your password'
+                label='Password'
+                value={password} onChangeText={text => setPassword(text)}
+                secureTextEntry
+            />
+            <Input
+                placeholder='Enter your image url'
+                label='Profile Picture'
+                value = {avatar}
+                onChangeText={text => setAvatar(text)}
+            />
+            <Button title='register' style={styles.button} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 100,
+    },
+    button: {
+        width: 370,
+        marginTop: 10
+    }
+});
+
+export default Register;
