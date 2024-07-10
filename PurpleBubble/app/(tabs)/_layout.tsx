dd8f1d5f2400f7377f062ca7c7b94a4a6e08ff00
Emperor Numerius <62899740+EmperorNumerius@users.noreply.github.com>
2024-07-09 18:07:32 -0600

    playing around with

--- PurpleBubble/app/(tabs)/_layout.tsx
+++ PurpleBubble/app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="MyPager"
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
@@ -24,7 +24,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="chatScreen"
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
